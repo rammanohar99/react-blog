4696561b89a317ee8bbb3e784cbc46b746bf413f
rammanohar99 <itsmanohar99@gmail.com>
2024-11-14 15:01:12 +0530

    feat: Add PostEditor and TextEditor components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import BlogList from './components/BlogList/BlogList';
 import { posts } from './data/posts';
+import PostEditor from './components/PostEditor/PostEditor'; // Import PostEditor
 import './App.css';
 
 function App() {
@@ -9,6 +10,8 @@ function App() {
       <Header />
       <main className="main-content">
         <BlogList posts={posts} />
+        {/* Render the PostEditor component */}
+        <PostEditor />
       </main>
     </div>
   );
